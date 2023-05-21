05712b0c12f9bb7a500149109d2023f2cc86c5d6
BADAL <badzkamli26@gmail.com>
2023-05-21 22:51:37 +0530

    First Commit

---
+++ src/api.js
@@ -0,0 +1,14 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f4b4fa02b4mshe3249cd50594814p1b0976jsn3c983c1db004',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "f9e18669ce3e1d1e389eff2ea3f928e4";
+
